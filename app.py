8b78941eb5c9108e5d3442daccc3171d31923009
Rob Thayer <theblack_apple@ymail.com>
2025-04-30 22:10:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def multiply():
     b = int(request.args.get('b', 1))
     return jsonify({'result': a * b})
 
+@app.route('/html', methods=['GET'])
+def writehtml():
+    index_page = '''<html><head><body><h2>Welcome to the page</body></head></html>'''
+    return index_page
+
 if __name__ == '__main__':
     app.run(debug=True)
